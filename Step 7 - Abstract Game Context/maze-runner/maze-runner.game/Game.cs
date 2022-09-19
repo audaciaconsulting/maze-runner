6ff4661efb37f55220a37d0afdf9d12941bd8cef
Esme-Lawrence <eslawrence906@gmail.com>
2022-09-19 15:45:14 +0100

    added step 7

---
+++ Step 7 - Abstract Game Context/maze-runner/maze-runner.game/Game.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+
+namespace maze_runner.game
+{
+    public class Game
+    {
+        public List<Sprite> Sprites = new List<Sprite>();
+
+        private List<GameObject> _gameObjects = new List<GameObject>();
+        private Sprite _block { get; set; }
+        private IGameContext _context;
+
+        public Game(IGameContext context)
+        {
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            _block = new Sprite
+            {
+                Source = "assets/whiteblock.png"
+            };
+
+            Sprites.Add(_block);
+
+            _gameObjects.Add(new GameObject { Sprite = _block, Colour = "red" });
+            _gameObjects.Add(new GameObject { Sprite = _block, Position = new Vector2(200, 0), Colour = "blue" });
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            foreach (var gameObject in _gameObjects)
+            {
+                _context.Draw(gameObject);
+            }
+        }
+    }
+}
---
+++ Step 7 - Abstract Game Context/maze-runner/maze-runner/BlazorGameContext.cs
@@ -0,0 +1,29 @@
+﻿using Blazor.Extensions.Canvas.Canvas2D;
+using maze_runner.game;
+
+namespace maze_runner
+{
+    public class BlazorGameContext : IGameContext
+    {
+        private readonly Canvas2DContext _context;
+
+        public BlazorGameContext(Canvas2DContext context)
+        {
+            _context = context;
+        }
+
+        public async void Draw(GameObject gameObject)
+        {
+            await _context.SaveAsync();
+
+            await _context.DrawImageAsync(
+               gameObject.Sprite.Ref,
+               gameObject.Position.X, gameObject.Position.Y);
+
+            await _context.RestoreAsync();
+
+            await _context.SetFillStyleAsync(gameObject.Colour);
+            await _context.FillRectAsync(gameObject.Position.X, gameObject.Position.Y, 100, 100);
+        }
+    }
+}
